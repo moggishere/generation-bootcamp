b87200f2a6caaf8c61192859a5426a8f68125083
moggishere <aryyoon@gmail.com>
2021-11-09 11:38:02 -0300

    adicionado o ex04

---
+++ module-I/java002/ex04.java
@@ -0,0 +1,23 @@
+// 4- Faça um programa em que permita a entrada de um número qualquer e exiba se este
+// número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for
+// ímpar exiba o número elevado ao quadrado.
+
+package Turma38Entrega2;
+import java.util.Scanner;
+public class ex04 {
+    public static void main(String[] args) {
+        double userInput;
+        double numResult;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nInsira um valor: ");
+        userInput = scan.nextInt();
+        scan.close();
+        if (userInput%2 == 0) {
+            numResult = Math.sqrt(userInput);
+            System.out.println("\nO número" + userInput +" é par e sua raiz quadrada é " + numResult);
+        } else if (userInput%2 != 0) {
+            numResult = Math.pow(userInput, 2);
+            System.out.println("\nO número" + userInput +" é ímpar e seu valor elevado ao quadrado é " + numResult);
+        }
+    }
+}
