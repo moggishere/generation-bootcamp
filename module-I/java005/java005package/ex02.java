d9ad2b4d0cf96a014a72a0e879bdf2b6b8361ddd
moggishere <aryyoon@gmail.com>
2021-11-12 19:43:41 -0300

    ex02, test

--- module-I/java005/java005package/ex02.java
+++ module-I/java005/java005package/ex02.java
@@ -1,7 +1,65 @@
 package java005package;
+
 // 2) Crie uma classe avião e apresente os atributos e métodos referentes
 // esta classe, em seguida crie um objeto avião, defina as instancias deste
 // objeto e apresente as informações deste objeto no console.
-public class ex02 {
-    
+public class ex02 { // planes
+    private int planeId;
+    private String planeName;
+    private String airlineCompany;
+    private int capacity;
+    private int year;
+
+    public ex02(int planeId, int year, int capacity, String planeName, String airlineCompany) {
+        this.planeId = planeId;
+        this.year = year;
+        this.capacity = capacity;
+        this.planeName = planeName;
+        this.airlineCompany = airlineCompany;
+    }
+
+    public void printPlane() {
+        System.out.println("\nID " + planeId + "\nYEAR " + year + "\nCAPACITY " + capacity + "\nNAME " + planeName + "\nAIRLINE " + airlineCompany);
+    }
+
+    public int getPlaneId() {
+        return this.planeId;
+    }
+
+    public void setPlaneId(int planeId) {
+        this.planeId = planeId;
+    }
+
+    public String getPlaneName() {
+        return this.planeName;
+    }
+
+    public void setPlaneName(String planeName) {
+        this.planeName = planeName;
+    }
+
+    public String getAirlineCompany() {
+        return this.airlineCompany;
+    }
+
+    public void setAirlineCompany(String airlineCompany) {
+        this.airlineCompany = airlineCompany;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 }
---
+++ module-I/java005/java005package/ex02Test.java
@@ -0,0 +1,10 @@
+package java005package;
+
+
+public class ex02Test {
+    public static void main(String[] args) {
+    ex02 plane001 = new ex02(1312, 1984, 150, "Batatinha", "LATAM");   
+    plane001.printPlane();
+    }
+    
+}
