3544c4b082bb11f2fce95c1394bced05e9488488
moggishere <aryyoon@gmail.com>
2021-11-18 15:13:20 -0300

    resolvido problemas com ex03

--- module-I/java006/Store/StoreMain.java
+++ module-I/java006/Store/StoreMain.java
@@ -9,6 +9,7 @@
 package Store;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.util.List;
 
@@ -17,35 +18,48 @@ public class StoreMain {
         int input;
         boolean on = true;
         Scanner scan = new Scanner(System.in);
+        Scanner scanString = new Scanner(System.in);
         List<String> catalog = new ArrayList<String>();
 
         do {
 
             System.out.println(
-                    "\nCATALOGUE:\n1 - Adicionar item\t2 - Remover item\t3 - Ordenar itens\t4 - Mostrar todos os items");
+                    "\nCATALOGUE:\n1 - Adicionar item\t2 - Remover item\t3 - Ordenar itens\t4 - Mostrar todos os items\t5 - SAIR");
             input = scan.nextInt();
 
             switch (input) {
             case 1:
-                System.out.println("\nDigite o nome do item que quer adicionar:");
-                catalog.add(scan.nextLine());
+                System.out.println("Digite o nome do item que quer adicionar:");
+                catalog.add(scanString.nextLine());
                 System.out.println("\nItem adicionado.");
                 break;
             case 2:
+                System.out.println("\nDigite o número do objeto que queira remover:");
+                for(int count = 0; count < catalog.size(); count ++) {
+                    System.out.println((count+1) + ". " + catalog.get(count));
+                }
+
+                int inputRemove = scan.nextInt() - 1;
+                catalog.remove(inputRemove);
 
                 break;
             case 3:
-
+                catalog.sort(Comparator.comparing(String::toString));
+                System.out.println("\nLista atualizada:\n" + catalog);
                 break;
             case 4:
-
+                System.out.println("\nOs itens adicionados foram:\n" + catalog);
+                break;
+            case 5:
+                System.out.println("\nSISTEMA ENCERRADO");
+                on = false;
                 break;
             default:
                 System.out.println("\nERROR");
             }
 
-        } while(on == true);
+        } while (on == true);
         scan.close();
-
+        scanString.close();
     }
 }
