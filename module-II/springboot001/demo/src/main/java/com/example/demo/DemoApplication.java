5cf305cd653781bc8d53b60a4a5ee7409160f734
moggishere <aryyoon@gmail.com>
2021-11-30 13:39:48 -0300

    adicionado hello world

--- module-II/springboot001/demo/src/main/java/com/example/demo/DemoApplication.java
+++ module-II/springboot001/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
+
+
+	String home() {
+		return "hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
